f25972b62922c320d53c197bf99029a06e0aa0c6
muhamadnss <muhammadnss.enterprise@gmail.com>
2020-12-16 19:04:45 +0700

    Update interface menu console dan deploy fitur (Exit Menu)

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+from selenium import webdriver #digunakan untuk import package webdriver dari library selenium
 import requests
 import bs4
 import os
@@ -6,10 +6,32 @@ import os
 #Define binding link
 top_track = "https://soundcloud.com/charts/top" #Digunakan untuk binding data track terbaik
 new_track = "https://soundcloud.com/charts/new" #Digunakan untuk binding data track terbaru
-track_query = "https://soundcloud.com/search/sounds?q=" #Digunakan untuk binding data daftar track berdasarkan query 
+track_query = "https://soundcloud.com/search/sounds?q="#Digunakan untuk binding data daftar track berdasarkan query 
 artist_query = "https://soundcloud.com/search/people?q=" #Digunakan untuk binding data daftar track berdasarkan query artist
 mix_url_end = "&filter.duration=epic"
 
 #Membuat objek web driver dengan selenium
 browser = webdriver.Chrome("E:\Learn\python\soundcloudWebScrapping\chromedriver.exe") #Define path driver
 browser.get("https://soundcloud.com") #Mulai melakukan binding pada soundcloud
+
+#Membangun menu console
+print()
+print(">>>>Selamat Datang di Simple Soundcloud Web Scrapper App<<<<<")
+print(">>>Anda dapat mencari berbagai track lagu terbaru dan terbaik dari semua genre disini<<<<")
+print()
+
+while True:
+    print(">>Menu Console<<")
+    print(">> 1 - Pencarian Track Lagu")
+    print(">> 2 - Pencarian Artis / Komposer Lagu")
+    print(">> 3 - Pencarian Mixed Lagu")
+    print(">> 4 - Pencarian Berdasarkan Daftar Tangga Lagu Teratas")
+    print(">> 4 - Pencarian Berdasarkan Daftar Tangga Lagu Terbaru dan Terpopuler")
+    print()
+
+    choice = int(input("Silahkan pilih fitur yang diinginkan : "))
+
+    if choice == 0:
+        browser.quit()
+        break
+    print()
