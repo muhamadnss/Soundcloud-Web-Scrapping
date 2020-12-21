967b0c4aa148e7796bbe889008d0d8207c283891
muhammadnss.enterprise@gmail.com <muhammadnss.enterprise@gmail.com>
2020-12-21 11:46:21 +0700

    Menambahkan fitur pemutaran track yang dipilih di fitur 4

--- scrapper.py
+++ scrapper.py
@@ -32,7 +32,7 @@ while True:
     print()
     choice = int(input("Silahkan pilih fitur yang diinginkan : "))
 
-    #Define fitur pencarian lagu berdasarkan judul
+    ##Define fitur pencarian lagu berdasarkan judul
     if choice == 1:
         name = input("Masukkan judul lagu yang ingin dicari : ")
         print()
@@ -40,7 +40,7 @@ while True:
         browser.get(track_query + name)
         continue
     
-    #Define fitur pencarian lagu berdasarkan artis
+    ##Define fitur pencarian lagu berdasarkan artis
     elif choice == 2:
         name = input("Masukkan nama artis yang ingin dicari : ")
         print()
@@ -48,48 +48,61 @@ while True:
         browser.get(artist_query + name)
         continue
 
-    #Define fitur pencarian lagu yang sudah remixed
+    ##Define fitur pencarian lagu yang sudah remixed
     elif choice == 3:
         name = input("Masukkan judul lagu remixed yang ingin dicari : ")
         "%20".join(name.split(" ")) #Ini dilakukan untuk melengkapi format keyword yang muncul pada setiap query
         browser.get(track_query + name + mix_url_end)
         continue
 
-    #Define fitur pencarian daftar lagu berdasarkan genre
+    ##Define fitur pencarian daftar lagu berdasarkan genre
     elif choice == 4:
         request = requests.get(top_track)
         soup = bs4.BeautifulSoup(request.text, "lxml")
-        #print(request.text) #Ini digunakan untuk testing get data chart song dari soundcloud
-        genres = soup.select("a[href*=genre]")[2:] #ini digunakan untuk filtering format raw data hasil retrieving dengan mengambil data yang terdapat pada tag <a> html
-        # for genre in genres:
-        #     print(genre) #menampilkan hasil data hasil retrieving
-        genre_links = []
-        for index, genre in enumerate(genres):
-            print(str(index) + ": " + genre.text)
-            genre_links.append(genre.get("href"))
-        print()
-        choice_2 = input(str("Masukkan pilihan anda (Ketik x jika ingin kembali ke menu sebelumnya): "))
-        print()
-        if choice_2 == "x": #handling CTA ke menu utama
-           continue
-        else:
-            choice = int(choice)
-        url = "https://soundcloud.com" + genre_links[choice] #Melakukan binding data ke direktori genre
-        requests = requests.get(url)
-        soup = bs4.BeautifulSoup(request.text, "lxml")
-        # print(request.text)
-        tracks = soup.select("h2")[3:] #Cleansing raw html untuk menampilkan data yang terdapat pada elemen <h2>  
-        # for track in tracks:
-        #     print(track) #Testing hasil retrieving data
-        #[INCREMENT 21 Dec 2020]
-        track_link = [] #Variabel sementara untuk menampung data link track dari elemen <a href>
-        track_names = [] #Variabel sementara untuk menampung data judul track dari elemen <a href>
-        for index, track in enumerate(tracks): #looping ini digunakan sebegai instrumen otomatisasi proses retrieving data
-            track_link.append(track.a.get("href")) #Memasukkan data link lagu ke array sementara
-            track_names.append(track.text) #Memasukkan data judul track ke array sementara
-            print(str(index + 1) + ": " + track.text)
+        while True:
+            print(">>Pilihan Genre : ")
             print()
-
+            #print(request.text) #Ini digunakan untuk testing get data chart song dari soundcloud
+            genres = soup.select("a[href*=genre]")[2:] #ini digunakan untuk filtering format raw data hasil retrieving dengan mengambil data yang terdapat pada tag <a> html
+            # for genre in genres:
+            #     print(genre) #menampilkan hasil data hasil retrieving
+            genre_links = []
+            for index, genre in enumerate(genres):
+                print(str(index) + ": " + genre.text)
+                genre_links.append(genre.get("href"))
+            print()
+            choice_2 = input(str("Masukkan pilihan anda (Ketik x jika ingin kembali ke menu sebelumnya): "))
+            print()
+            if choice_2 == "x": #handling CTA ke menu utama
+            continue
+            else:
+                choice = int(choice)
+            url = "https://soundcloud.com" + genre_links[choice] #Melakukan binding data ke direktori genre
+            requests = requests.get(url)
+            soup = bs4.BeautifulSoup(request.text, "lxml")
+            # print(request.text)
+            tracks = soup.select("h2")[3:] #Cleansing raw html untuk menampilkan data yang terdapat pada elemen <h2>  
+            # for track in tracks:
+            #     print(track) #Testing hasil retrieving data
+            ###[INCREMENT 21 Dec 2020]
+            track_link = [] #Variabel sementara untuk menampung data link track dari elemen <a href>
+            track_names = [] #Variabel sementara untuk menampung data judul track dari elemen <a href>
+            for index, track in enumerate(tracks): #looping ini digunakan sebegai instrumen otomatisasi proses retrieving data
+                track_link.append(track.a.get("href")) #Memasukkan data link lagu ke array sementara
+                track_names.append(track.text) #Memasukkan data judul track ke array sementara
+                print(str(index + 1) + ": " + track.text)
+                print()
+            ##Fitur pemutaran track yang sudah dipilih
+            while True:
+                choice = input("Masukkan pilihan lagu yang ingin diputar (Masukkan x jika ingin kembali ke menu sebelumnya: ")
+                print()
+                if choice == "x":
+                    break
+                else:
+                    choice = int(choice) - 1
+                print("Now Playing : " + track_names[choice])
+                print()
+                browser.get("https://soundclpud.com" + track_link[choice])
     #Define exit menu console
     elif choice == 0:
         browser.quit()
